cefc5f4c1e5d62060f923a43bccc6ffb5217a3f5
boshrajumaa <busra.cuma@ogr.sakarya.edu.tr>
2025-12-12 19:14:44 +0300

    ApplicationDbContext.cs

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,19 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore; // المكتبة الجديدة
+using Microsoft.EntityFrameworkCore;
 using FitnessCenterApp.Models;
 
 namespace FitnessCenterApp.Data
 {
-    // Veritabanı bağlam sınıfı (Veritabanı ile iletişim kuran köprü)
-    public class ApplicationDbContext : DbContext
+    // IdentityDbContext sınıfından türetiyoruz ki Kullanıcı ve Rol tabloları gelsin
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
 
-        // Veritabanı tablolarımızı temsil eden DbSet tanımları
-        // Bu kodlar, C# sınıflarını SQL tablolarına dönüştürür
-        public DbSet<Trainer> Trainers { get; set; }     // Eğitmenler Tablosu
-        public DbSet<Service> Services { get; set; }     // Hizmetler Tablosu
-        public DbSet<Appointment> Appointments { get; set; } // Randevular Tablosu
+        public DbSet<Trainer> Trainers { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
     }
 }
